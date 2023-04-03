38e2c9eae6054bd35635df7d46f505bf79b2be51
juan.avalon4@gmail.com <juan.avalon4@gmail.com>
2023-04-03 12:49:02 -0600

    mode

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+var animation = anime({
+ targets:'.png',
+ translateY: -5,
+ direction: 'alternate',
+ duration: 1000,
+ loop: true,
+ easing: 'easeInOutQuad'
+});
+
+
+var animation = anime({
+ targets:'.iwatch',
+ translateX: 300,
+ duration: 1000,
+ 
+});
+
+
+var animation = anime({
+ targets:'.change',
+ translateX: -150,
+ duration: 1000,
+ 
+});
+
